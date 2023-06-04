8c83fbc6606f77658e66cd49468459969987fc2e
keshav0888 <76722805+keshav0888@users.noreply.github.com>
2023-06-04 22:51:45 +0530

    c

--- flipkart/src/App.js
+++ flipkart/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Flipkart from "./image/flipkart.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <div className="top-bar">
+        <div className="item top-bar-start"></div>
+        <div className="item top-bar-img">
+          <img className="img-top-img" src={Flipkart}></img>
+        </div>
+        <div className="item top-bar-input">
+          <input
+            className="input-top-bar"
+            placeholder="Search For products, brands, and more"
+          />
+        </div>
+        <div className="item top-bar-img-button">
+          <button className="button-top-bar">Login</button>
+        </div>
+        <div className="item top-bar-img-seller">
+          <span>Become a Seller</span>
+        </div>
+        <div className="item top-bar-img-more">
+          <span>More</span>
+        </div>
+        <div className="item top-bar-img-cart">
+          <span>Cart</span>
+        </div>
+        <div className="item top-bar-end"></div>
+      </div>
     </div>
   );
 }
